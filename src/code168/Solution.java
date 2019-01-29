8d3b225c7ddaa02457c91140cffc53b261aa333e
gopros <1083632684@qq.com>
2019-01-29 17:14:17 +0800

    2019

---
+++ src/code168/Solution.java
@@ -0,0 +1,25 @@
+package code168;
+
+
+class Solution {
+
+    public String convertToTitle(int n) {
+
+        int t = n/26;
+        int p = n%26;
+
+        char[] s = {'A','B','C','D','E','F'};
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+
+    }
+
+
+
+}
