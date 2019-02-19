dc1302ca7965ac75f3ddf4532b97b26e025d77e5
gopros <1083632684@qq.com>
2019-02-19 16:57:11 +0800

    2019

--- src/code97/Solution.java
+++ src/code97/Solution.java
@@ -5,16 +5,42 @@ class Solution {
 
     public boolean isInterleave(String s1, String s2, String s3) {
 
-        for (int i = 0; i < s3.length(); i++) {
+        if (s1.length()+s2.length()!=s3.length()) return false;
 
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+
+        dp[0][0] = true;
+        for (int i = 1; i <= s1.length(); i++) {
+            if (s1.charAt(i-1)==s3.charAt(i-1)){
+                dp[i][0] = dp[i-1][0];
+            }
         }
-        return true;
+        for (int i = 1; i <= s2.length(); i++) {
+            if (s2.charAt(i-1)==s3.charAt(i-1)){
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (i+j<=s3.length()){
+                    if (s3.charAt(i+j-1)==s1.charAt(i-1)){
+                        dp[i][j] = dp[i-1][j];
+                    }
+                    if (s3.charAt(i+j-1)==s2.charAt(j-1)){
+                        dp[i][j] = dp[i][j]||dp[i][j-1];
+                    }
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
     }
 
     public static void main(String[] args) {
 
         Solution s = new Solution();
-        s.isInterleave("aabcc","dbbca","aadbbcbcac");
+        System.out.println(s.isInterleave("ab","bc","babc"));
 
 
     }
--- src/test/test.java
+++ src/test/test.java
@@ -7,33 +7,11 @@ import java.nio.ByteBuffer;
 
 public class test {
 
-    public void swap(int a,int b){
-        int c = a;
-        a = b;
-        b = c;
-        return;
 
-    }
+
+
 
     public static void main(String[] args) {
-
-
-        test t =new test();
-        int a=1,b=2;
-        t.swap(1,2);
-        System.out.println(a+" "+b);
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
